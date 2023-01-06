8b121ad2dcbce02fafebae73bcc15f0c41573733
KAABECHE rayane <rayanoo1302@gmail.com>
2023-01-06 06:05:55 +0100

    test

--- Graphics/DominoGridGraphics.cpp
+++ Graphics/DominoGridGraphics.cpp
@@ -9,10 +9,11 @@ DominoGridGFX::DominoGridGFX() {}
 
 
 void DominoGridGFX::drawGridDominoTuile (sf::RenderWindow &window, dominoGrid grid) {
-    for (int i = 0; i < grid.getWidth(); i++) {
-        for (int j = 0; j < grid.getHeight(); j++) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             DominoTuileGFX dominoTuileGFX = DominoTuileGFX(grid.getDominoTuile(i,j), i * 50, j * 50);
             window.draw(dominoTuileGFX);
+            dominoTuileGFX.setTextPosition(window);
         }
     }
 }
