d43544228943ad16e12c0a37c00375273ec6db1b
KAABECHE rayane <rayanoo1302@gmail.com>
2023-01-06 03:20:09 +0100

    test

--- Graphics/traxGridGraphics.cpp
+++ Graphics/traxGridGraphics.cpp
@@ -20,11 +20,11 @@ void traxGridGraphics::traxGridGraphics::renderGG(sf::RenderWindow& window,
     for (int y = 0; y < BOARD_HEIGHT; y++) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
             TraxTuile tile = grid.tiles[y][x];
-
+            std::cout << "x: " << tile.getX() << " y: " << tile.getY() << std::endl;
             // Create a sprite for the tile
             sf::Sprite sprite;
-            sprite.setPosition(tile.getX() * TILE_SIZE,
-                               tile.getY() * TILE_SIZE);
+            sprite.setPosition(x * TILE_SIZE,
+                               y * TILE_SIZE);
 
             // Set the texture of the sprite based on the state of the tile
             if (tile.state == TileState::Empty) {
@@ -33,7 +33,7 @@ void traxGridGraphics::traxGridGraphics::renderGG(sf::RenderWindow& window,
             } else {
                 sprite.setTexture(tuileGraphics.textures[1]);
             }
-
+            
             window.draw(sprite);
         }
     }
